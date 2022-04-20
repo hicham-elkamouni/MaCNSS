2f513525b8a8174949c92118541d9dbe5dad38bd
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-20 15:22:34 +0000

    addign agent model

--- server/src/interfaces/index.ts
+++ server/src/interfaces/index.ts
@@ -1,1 +1,1 @@
-export default {};
+export { IAgent } from "./mongoose.types"
--- server/src/interfaces/mongoose.types.ts
+++ server/src/interfaces/mongoose.types.ts
@@ -4,6 +4,8 @@ export interface IAgent extends Document {
     userName: string;
     email: string;
     hashed_password: string;
+    salt: string;
+    authenticate: Function;
     createdAt:Date
     updatedAt:Date
 }
