7b235306d26e2282c4d98e25aad11d3c6595e69b
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-20 12:19:55 +0000

    setup database and making first call is done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,29 @@
-import 'dotenv/config';
-import express, { Response, Request } from 'express';
-import compression from 'compression';
-import { limiter } from '@middlewares/limiter';
-import helmet from 'helmet';
+import dotenv from "dotenv";
+import express, { Response, Request } from "express";
+import { connectDB } from '@config/database';
+import compression from "compression";
+import { limiter } from "@middlewares/limiter";
+import helmet from "helmet";
 
 const app = express();
-
+// change default environment path
+dotenv.config({ path: "./.env" });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(helmet());
 app.use(limiter);
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hi Mom 👋',
-  });
-});
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+const host = process.env.APP_HOSTNAME || "localhost";
+const url = process.env.APP_URL || `http://${host}:${port}`;
 
 // listen to port
 app.listen(port, async () => {
-  console.log(`🚀 Server ready at: http://localhost:${port}`);
+
+  // connect to database
+  const { connection } = await connectDB();
+  console.log(`👋 Connected to database successfully: ${connection.name}`);
+  console.log(`🚀 Server ready at: ${url}`);
+
 });
